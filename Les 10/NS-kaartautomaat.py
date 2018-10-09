eba9e204d7c207f7356b7e20364608f85811e8c8
basb2801 <bas.bloemhof@student.hu.nl>
2018-10-09 14:18:13 +0200

    De Grote NS Kaarautomaat.

--- Les 10/NS-kaartautomaat.py
+++ Les 10/NS-kaartautomaat.py
@@ -9,9 +9,9 @@ def inlezen_beginstation(stations):
 
 def inlezen_eindstation(stations, beginstation):
     'Vraag om eindstation'
-    eindstation = input('Wat is je eindstation?')
     while True:
-        if eindstation in stations:
+        eindstation = input('Wat is je eindstation?')
+        if eindstation in stations and stations.index(beginstation) < stations.index(eindstation):
             break
     return eindstation
 
@@ -20,10 +20,20 @@ def omroepen_reis(stations, beginstation, eindstation):
     'Print omroep en prijs voor rit'
     afstand = stations.index(eindstation) - stations.index(beginstation)
     prijs = afstand * 5
-    print('Het beginstation {} is het {}e station in het traject'.format(beginstation, stations.index(beginstation)))
-    print('Het eindstation {} is het {}e station in het traject'.format(eindstation, stations.index(eindstation)))
+    beginindex = stations.index(beginstation) + 1
+    print('Het beginstation {} is het {}e station in het traject'.format(beginstation, stations.index(beginstation)+1))
+    print('Het eindstation {} is het {}e station in het traject'.format(eindstation, stations.index(eindstation)+1))
     print('De afstand bedraagt {} station(s).'.format(afstand))
     print('De prijs van het kaartje is {} euro.'.format(prijs))
+    print('Je stapt in de trein in: {}'.format(beginstation))
+    while True:
+        if beginindex == stations.index(eindstation):
+            break
+        else:
+            print(stations[beginindex])
+            beginindex = beginindex + 1
+    print('Je stapt uit in {}'.format(eindstation))
+
 
 
 stations = ['Schagen', 'Heerhugowaard', 'Alkmaar', 'Castricum', 'Zaandam', 'Amsterdam Sloterdijk', 'Amsterdam Centraal',
